0eac4f3f8babaab42b5e43e8763ef5b73a839cb5
AlexDjon <alexdjonata30@gmail.com>
2020-12-04 11:46:42 -0300

    Apagando o build do repositório e alterando o .gitignore

--- backend/build/routes/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var Router = require('express').Router;
-var router = Router();
-var ManifestsRoutes = require('./manifests.routes');
-router.use('/manifests', ManifestsRoutes);
-module.exports = router;
--- backend/build/services/marsApi.service.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var axios = require('axios');
-var api = axios.create({
-    baseURL: "https://api.nasa.gov/mars-photos/api/v1",
-    params: {
-        api_key: "9JzCItqyddRdjUAwltjVyL5YwQF5D2Y6WW8Xc9dP"
-    }
-});
-module.exports = api;
