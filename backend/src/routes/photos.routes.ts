ff6a5286f0686ca4d6f8ad5395b12bb81dda7900
AlexDjon <alexdjonata30@gmail.com>
2020-12-06 18:11:31 -0300

    Routes -> Mudando o método para realizar a sincronização

--- backend/src/routes/photos.routes.ts
+++ backend/src/routes/photos.routes.ts
@@ -1,8 +1,11 @@
 const { Router } = require('express');
+import PhotosController from "../controllers/photos.controller";
 import MarsIntegration from "../fixtures/mars.integration";
 
 const PhotosRoutes = Router();
 
-PhotosRoutes.get('/sync', MarsIntegration.sync_photos);
+PhotosRoutes.get('/period', PhotosController.getByPeriod);
+
+PhotosRoutes.post('/sync', MarsIntegration.sync_photos);
 
 export default PhotosRoutes;
