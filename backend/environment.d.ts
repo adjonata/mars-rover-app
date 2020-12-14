168c2c92b88a40df5eaa98aaa6b421c946d9b359
adjonata <alexdjonata30@gmail.com>
2020-12-14 16:54:52 -0300

    Auth -> Desenvolvendo sistema de autenticação

--- backend/environment.d.ts
+++ backend/environment.d.ts
@@ -5,7 +5,8 @@ declare global {
     interface ProcessEnv {
       PRODUCTION_PORT: number,
       CONNECTION: string,
-      NODE_ENV: 'production' | 'development'
+      NODE_ENV: 'production' | 'development',
+      SECRET: string
     }
   }
 }
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from "express";
 
 import ManifestsRoutes from "./manifests.routes";
 import PhotosRoutes from "./photos.routes";
+import AuthRoutes from "./auth.routes";
 
 const router = Router();
 
 router.use("/manifests", ManifestsRoutes);
 router.use("/photos", PhotosRoutes);
+router.use("/auth", AuthRoutes);
 
 export default router;
