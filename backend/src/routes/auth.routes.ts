f805b11363e1e6a28a843946013db9ace6189114
alexdjonata <alexdjonata30@gmail.com>
2020-12-28 11:39:29 -0300

    Celebrate -> Iniciando validação de rotas

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -5,9 +5,15 @@ const AuthRoutes = Router();
 import { noLogged } from "../middlewares/auth";
 
 import AuthController from "../controllers/auth.controller";
+import validation from "../validation/auth.valid";
 
-AuthRoutes.post("/register", noLogged, AuthController.register);
-AuthRoutes.post("/login", noLogged, AuthController.login);
+AuthRoutes.post(
+  "/register",
+  noLogged,
+  validation.register,
+  AuthController.register
+);
+AuthRoutes.post("/login", noLogged, validation.login, AuthController.login);
 AuthRoutes.get("/logout", AuthController.logout);
 
 export default AuthRoutes;
