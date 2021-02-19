e3b35bbd8ecd376bbb05e34a7004e886cfadbfac
adjonata <alexdjonata30@gmail.com>
2021-02-18 22:16:45 -0300

    feat: manifests types

---
+++ backend/src/types/Cameras.ts
@@ -0,0 +1,12 @@
+type Cameras =
+  | "FHAZ"
+  | "RHAZ"
+  | "MAST"
+  | "CHEMCAM"
+  | "MAHLI"
+  | "MARDI"
+  | "NAVCAM"
+  | "PANCAM"
+  | "MINITES";
+
+export default Cameras;
---
+++ backend/src/types/index.ts
@@ -0,0 +1,5 @@
+import Photo from "./Photo";
+import Cameras from "./Cameras";
+import ManifestsResponse from "./ManifestsResponse";
+
+export { Photo, Cameras, ManifestsResponse };
