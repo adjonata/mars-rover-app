51cd7061cb3f4e0afc0aca641173983fc31c09d6
adjonata <alexdjonata30@gmail.com>
2021-02-18 22:15:32 -0300

    feat: update environment

--- backend/environment.d.ts
+++ backend/environment.d.ts
@@ -1,17 +1,12 @@
-import winston from "winston";
-
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PRODUCTION_PORT: number,
-      CONNECTION: string,
-      NODE_ENV: 'production' | 'development',
-      SECRET: string
+      PRODUCTION_PORT: number;
+      CONNECTION: string;
+      NODE_ENV: "production" | "development";
+      SECRET: string;
     }
   }
 }
 
-declare const winston: winston.Winston;
-export = winston;
-
-export {}
+export {};
