17ff557ccd1ce75df483211a0da49aed93938be7
adjonata <alexdjonata30@gmail.com>
2021-02-18 22:26:56 -0300

    feat: removing frontend and moving backend to source

--- frontend/src/plugins/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from "vue";
-
-import VCalendar from "v-calendar";
-import Notifications from "vue-notification";
-
-Vue.use(VCalendar, {
-  componentPrefix: "vc"
-});
-Vue.use(Notifications);
--- frontend/src/services/constants.js
+++
@@ -1,6 +0,0 @@
-export default {
-  latest_images: "/rovers/curiosity/latest_photos",
-  manifests: "/manifests/curiosity",
-  martian_sol: "/rovers/curiosity/photos",
-  earth_date: "/rovers/curiosity/photos"
-};
