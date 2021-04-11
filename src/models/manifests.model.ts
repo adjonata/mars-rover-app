ced44a2baeddfe17ebece44d68211651e12ed17f
adjonata <alexdjonata30@gmail.com>
2021-04-10 22:19:55 -0300

    fix: update models

--- src/models/manifests.model.ts
+++ src/models/manifests.model.ts
@@ -1,17 +1,13 @@
 import mongoose, { Schema, Document } from "mongoose";
+import PhotosManifest from "@/types/PhotosManifest";
 
-export interface IManifest extends Document {
-  sol: number,
-  earth_date: Date | string,
-  total_photos: number,
-  cameras: string[],
-}
+export interface IManifest extends PhotosManifest, Document {}
 
-const Manifests = new Schema({
+const ManifestsModel = new Schema({
   sol: Schema.Types.Number,
   earth_date: Schema.Types.Date,
   total_photos: Schema.Types.Number,
   cameras: [String]
 });
 
-export default mongoose.model<IManifest>("Manifests", Manifests);
+export default mongoose.model<IManifest>("Manifests", ManifestsModel);
