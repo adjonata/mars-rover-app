c55293e1cecfa7b15372eecfe96fc9c09cc5e3f4
adjonata <alexdjonata30@gmail.com>
2021-04-18 11:09:11 -0300

    fix: changes in auth controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,56 +3,67 @@ import { Request, Response } from "express";
 import * as crypt from "@/utils/crypt";
 import jwt from "jsonwebtoken";
 
+interface LoginRequest extends Request {
+  body: {
+    password: string;
+    email: string;
+  };
+}
+interface RegisterRequest extends Request {
+  body: {
+    email: string;
+    password: string;
+    username: string;
+  };
+}
+
 export default {
-  async login(req: Request, res: Response) {
-    const { email, password }: IAuthLogin = req.body;
+  async login(request: LoginRequest, response: Response) {
+    const { email, password } = request.body;
 
     if (!email || !password) {
-      return res.status(401).json({
+      return response.status(401).json({
         message: "Invalid login."
       });
     }
 
-    return await Auth.findOne({ email })
-      .then(async user => {
-        if (user) {
-          const comparePassword = await crypt.compare(password, user.password);
+    const user = await Auth.findOne({ email });
 
-          if (comparePassword) {
-            const { _id: id, email, username } = user;
-            const token = jwt.sign({ id }, String(process.env.SECRET), {
-              expiresIn: 7200
-            });
-
-            return res.status(200).json({
-              user: {
-                id,
-                email,
-                username
-              },
-              token
-            });
-          } else {
-            return res.status(400).json({
-              message: "Invalid password"
-            });
-          }
-        } else {
-          return res.status(404).json({
-            message: "User not found."
-          });
-        }
-      })
-      .catch(err => {
-        return res.status(500).json(err);
+    if (!user) {
+      return response.status(404).json({
+        message: "User not found."
       });
+    }
+
+    const comparePassword = await crypt.compare(password, user.password);
+
+    if (!comparePassword) {
+      return response.status(400).json({
+        message: "Invalid password"
+      });
+    }
+
+    const { _id: id, username } = user;
+
+    const token = jwt.sign({ id }, String(process.env.SECRET), {
+      expiresIn: 7200
+    });
+
+    return response.status(200).json({
+      user: {
+        id,
+        email,
+        username
+      },
+      token
+    });
   },
 
-  async register(req: Request, res: Response) {
-    const { email, password, username }: IAuth = req.body;
+  async register(request: RegisterRequest, response: Response) {
+    const { email, password, username } = request.body;
 
     if (!email || !password || !username) {
-      return res.status(401).json({
+      return response.status(401).json({
         message: "Invalid register."
       });
     }
@@ -66,26 +77,24 @@ export default {
     });
 
     if (!passwordHash) {
-      return res.status(501).json({
+      return response.status(501).json({
         message: "Problems in generate password hash"
       });
     }
 
-    const toCreate = {
-      email,
-      password: passwordHash,
-      username
-    };
+    const findOneAlreadyInUse = await Auth.findOne({ email });
 
-    const findFirst = await Auth.find({ email, username });
-
-    if (findFirst.length > 0) {
-      return res.status(403).json({
-        message: "This email or username is already in use"
+    if (findOneAlreadyInUse) {
+      return response.status(403).json({
+        message: "This email is already in use."
       });
     }
 
-    return await Auth.create(toCreate)
+    return await Auth.create({
+      email,
+      password: passwordHash,
+      username
+    })
       .then(async user => {
         const { id, email, username } = user;
 
@@ -93,7 +102,7 @@ export default {
           expiresIn: 7200
         });
 
-        return res.status(200).json({
+        return response.status(200).json({
           user: {
             id,
             email,
@@ -103,10 +112,10 @@ export default {
         });
       })
       .catch(err => {
-        return res.status(500).json(err);
+        return response.status(500).json(err);
       });
   },
-  logout(req: Request, res: Response) {
-    return res.json({ auth: false, token: "" });
+  logout(request: Request, response: Response) {
+    return response.json({ auth: false, token: "" });
   }
 };
