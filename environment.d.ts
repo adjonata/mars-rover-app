e77d357be61f9affc4f6807a6957b635bceea6c2
adjonata <alexdjonata30@gmail.com>
2021-04-18 11:07:47 -0300

    fix: remove environment variable

--- environment.d.ts
+++ environment.d.ts
@@ -1,7 +1,6 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      PRODUCTION_PORT: number;
       CONNECTION: string;
       NODE_ENV: "production" | "development";
       SECRET: string;
